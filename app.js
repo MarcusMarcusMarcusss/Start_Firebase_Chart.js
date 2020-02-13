c25c45066042c38016a645a9ebfafa94f371b4d4
MarcusMarcusMarcusss <53335641+MarcusMarcusMarcusss@users.noreply.github.com>
2020-02-13 11:31:47 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDEDUo0zFehNqlvpvuilwscKMdD6P15_GY",
+    authDomain: "example-9173c1.firebaseapp.com",
+    databaseURL: "https://example-9173c1.firebaseio.com/",
+    projectId: "example-9173c1",
+    storageBucket: "https://example-9173c1.firebaseio.com/",
+    messagingSenderId: "1038016092802"
+};
+firebase.initializeApp(config);
